8f1b0305c9ff57772a3a73a56e4cc2e7775fc047
Damien Coney <Damien Coney>
2015-11-19 00:17:11 +0000

    Main screen flow implemented. Next commit will contain the other pages with limited functionality.

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -18,28 +18,16 @@
     {
         /* your code goes here */ 
     });
-    
-        /* button  #submitFirstStageProcessButton */
-    
-    
-        /* button  #startButton */
-    
      
         /* button  #submitItemChoice */
     $(document).on("click", "#submitItemChoice", function(evt)
     {
-        alert("Amazon: " + localStorage.getItem("amazonAPISearch"));
-        alert("Ebay: " + localStorage.getItem("ebayAPISearch"));
-        alert("Expansys: " + localStorage.getItem("expansysAPISearch"));
         
         if($('#itemInput').val() === ""){
             alert("Please enter a value into the search box!");
         } else {
             localStorage.setItem('searchTerm', $('#itemInput').val());
             $('#goToVerifyProcess').trigger('click');
-            /*activate_page('#itemVerifyProcess');*/
-            /*$(document).loadContent("#itemVerifyPage",false,false,"fade");*/
-            /*$.mobile.changePage($("#itemVerifyPage"));*/
         }
     });
     
@@ -57,9 +45,6 @@
         /* button  #submitFirstStageProcessButton */
     $(document).on("click", "#submitFirstStageProcessButton", function(evt)
     {
-        console.log($('#amazonCheckbox').prop("checked"));
-        console.log($('#ebayCheckbox').prop("checked"));
-        console.log($('#expansysCheckbox').prop("checked"));
         if(!$('#amazonCheckbox').prop("checked" )&&
            !$('#ebayCheckbox').prop("checked") &&
            !$('#expansysCheckbox').prop("checked")) {
@@ -70,17 +55,23 @@
             localStorage.setItem("ebayAPISearch", $('#ebayCheckbox').prop("checked"));
             localStorage.setItem("expansysAPISearch", $('#expansysCheckbox').prop("checked"));
             $('#goToChoicePage').trigger('click');
-            /*activate_page('#itemChoicePage');*/
         }
     });
-    
-        /* button  #submitPriceButton */
+        
+    /* button  #submitPriceButton */
     $(document).on("click", "#submitPriceButton", function(evt)
     {
-        /* your code goes here */ 
+         /*global activate_page */
+        alert("I have been clicked!");
         $('#goToThankYouPage').trigger('click');
     });
     
-}
+        /* button  #mainMenuButton */
+    $(document).on("click", "#mainMenuButton", function(evt)
+    {
+        $("#backToMainMenuFromLastPage").trigger('click');
+    });
+    
+    }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
